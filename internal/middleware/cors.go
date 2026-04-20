0d92cfc0d3d326fe6f2b97ca40da9045321b48c4
David Akin <akindav16@gmail.com>
2026-04-20 11:53:29 +0100

    fix:fixed cors error on patch requests

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -10,7 +10,7 @@ func CORSMiddleware(next http.Handler) http.Handler {
 		if origin == "http://localhost:3000" || origin == "https://paysim-sigma.vercel.app" {
 			w.Header().Set("Access-Control-Allow-Origin", origin)
 		}
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
 		// 🔥 Handle preflight request
