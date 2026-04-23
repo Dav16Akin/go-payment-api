e9144d6ad122fbf8d4b3a171359c1a1b01c28e92
David Akin <akindav16@gmail.com>
2026-04-23 14:50:26 +0100

    fix:still fixing cors errors

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -7,12 +7,18 @@ func CORSMiddleware(next http.Handler) http.Handler {
 
 		origin := r.Header.Get("Origin")
 
-		if origin == "http://localhost:3000/" || origin == "https://paysim-sigma.vercel.app" {
+		allowedOrigins := map[string]bool{
+			"http://localhost:3000": true,
+			"https://paysim-sigma.vercel.app": true,
+		}
+		
+		if allowedOrigins[origin] {
 			w.Header().Set("Access-Control-Allow-Origin", origin)
 		}
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH")
 
 		// 🔥 Handle preflight request
 		if r.Method == http.MethodOptions {
